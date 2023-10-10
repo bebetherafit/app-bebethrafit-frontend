bbe1205528032b565849b94f512664fda84c40f9
Junho <jhshin9345@gmail.com>
2023-10-10 21:50:26 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// 출력 테스트
+function App() {
+    return <div>안녕하세요</div>;
+    }
+    
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import App from "./App";
+import { createRoot } from "react-dom";
+
+// 출력 테스트
+createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+
+    </React.StrictMode>
+    );
