851f016266951e89815bffbe9ac57de9470ae311
jhshin9345@gmail.com <jhshin9345@gmail.com>
2023-10-17 03:12:13 +0900

    route update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import LoginPage from './pages/loginPage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
   );
