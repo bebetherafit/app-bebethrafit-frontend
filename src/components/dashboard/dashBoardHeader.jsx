736bf956a4515085a00273c1fb81889bfe2c3ee8
Junho <jhshin9345@gmail.com>
2023-10-19 20:55:57 +0900

    updaetd dash header

--- src/components/dashboard/dashBoardHeader.jsx
+++ src/components/dashboard/dashBoardHeader.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import '../../styles/dashboard.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 function DashBoardHeader() {
     return (
-        <div className="header-bar">
-            <FontAwesomeIcon icon={faBell} />
-            <div className="alert-circle"></div>
+        <div className="dashBoardHeader">
+            <FontAwesomeIcon icon={faBell} className='bells' />
         </div>
     );
 }
