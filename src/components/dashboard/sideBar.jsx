72e000c4fdad5619970749141c71fbe1db599b09
Junho <jhshin9345@gmail.com>
2023-10-19 17:28:25 +0900

    updated sidebar

--- src/components/dashboard/sideBar.jsx
+++ src/components/dashboard/sideBar.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/sidebar.css';
+import profilePic from '../../assets/bebeimg.png';
 import { Link } from 'react-router-dom';
 
-
 const Sidebar = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [clinic, setClinic] = useState('');
+
+    useEffect(() => {
+        fetch('/api/user')
+            .then(response => response.json())
+            .then(data => {
+                setName(data.name);
+                setEmail(data.email);
+                setClinic(data.clinic);
+            })
+            .catch(error => console.error(error));
+    }, []);
+
     return (
         <div className="sidebar">
-            <div className="sidebar-header">
-                <h3>나의 분석 리포트</h3>
+            <div className="sidebarHeader">
+                <h2>나의 분석 리포트</h2>
             </div>
-            <div className="sidebar-account">
-                <div>
-                <img src="https://via.placeholder.com/50" alt="user avatar" />
-                </div>
-                <div className="sidebar-account-info">
-                    <p>John Doe</p>
-                    <span>Admin</span>
+            <div className="sidebarAccount">
+                <div className="sidebarAccountInfo">
+                    <img src={profilePic} width={50} alt="user avatar" />
+                    <div className='userInfo'>
+                        <p>{name} | {clinic}</p>
+                        <p>{email}</p>
+                    </div>
                 </div>
             </div>
-            <div className="sidebar-menus">
+            <div className="sidebarMenus">
                 <ul>
                     <li>
                         <Link to="#">분석 리포트</Link>
@@ -33,7 +48,7 @@ const Sidebar = () => {
                         <Link to="#">커뮤니티</Link>
                     </li>
                     <li>
-                        <Link to="#">Menu 5</Link>
+                        <Link to="#">고객센터</Link>
                     </li>
                 </ul>
             </div>
