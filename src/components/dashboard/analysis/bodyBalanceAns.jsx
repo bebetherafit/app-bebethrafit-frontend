e09a2e32bc3fa057e16d3c7aeb0088d324b96a21
Junho <jhshin9345@gmail.com>
2023-10-21 16:31:24 +0900

    updated analysis view

--- src/components/dashboard/analysis/bodyBalanceAns.jsx
+++ src/components/dashboard/analysis/bodyBalanceAns.jsx
@@ -1,33 +1,46 @@
 import React from 'react';
+import bodyBalanceImg from '../../../assets/human-icon.png';
 
-const BodyBalance = ({ leftPercentage, rightPercentage}) => {
-    leftPercentage = -0.127;
-    rightPercentage = +0.123; 
-    const unbalValue = leftPercentage - rightPercentage;
-    
+const BodyBalance = ({ leftPercentage, rightPercentage }) => {
+    leftPercentage = (-0.127 * 100).toFixed(2);
+    rightPercentage = (+0.123 * 100).toFixed(2);
+    const unbalValue = (leftPercentage - rightPercentage).toFixed(2);
+
     return (
-        <div className='bodyBalanceContainter' style={{}}>
+        <div className='bodyBalanceContainter'>
             <h4>신체 불균형</h4>
-            <div className='bodyBalnceData' style={{display: 'flex', flexDirection: 'row'}}>
+            <div className='bodyBalnceData' style={{ display: 'flex', flexDirection: 'row'}}>
                 <div className='bodyBalanceGraphic'>
-                    <img src="/path/to/body_balance.png" alt="bodyBalance" />
+                    <img src={bodyBalanceImg} width={'40%'} alt="bodyBalance" />
                 </div>
-                <table className='BalanceData'>
-                    <thead>
-                        <tr>
-                            <th style={{ backgroundColor: '#F7685B', borderRadius: '5px', color: '#ffffff', width: '15px' }}>왼발</th>
-                            <th style={{ backgroundColor: '#2ED47A', borderRadius: '5px', color: '#ffffff', width: '15px' }}>오른발</th>
-                        </tr>
-                        <tr>
-                            <td>{leftPercentage}%</td>
-                            <td>{rightPercentage}%</td>
-                        </tr>
-                        <tr>
-                            <td><h3>불균형</h3></td>
-                            <td>{unbalValue}</td>
-                        </tr>
-                    </thead>
-                </table>
+                <div style={{ width: '80%' }}>
+                    <table className='BalanceData' style={{ width: '55%', textAlign: 'center', borderSpacing: '10px' }}>
+                        <colgroup>
+                            <col style={{ width: '10%' }} /> {/* 빈 열 추가 */}
+                            <col style={{ width: '40%' }} />
+                            <col style={{ width: '40%' }} />
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <th style={{ backgroundColor: 'transparent', verticalAlign: 'middle' }}></th> {/* 빈 열 추가 */}
+                                <th style={{ backgroundColor: '#F7685B', borderRadius: '5px', color: '#ffffff', verticalAlign: 'middle' }}>왼발</th>
+                                <th style={{ backgroundColor: '#2ED47A', borderRadius: '5px', color: '#ffffff', verticalAlign: 'middle' }}>오른발</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td style={{ verticalAlign: 'middle' }}></td> {/* 빈 열 추가 */}
+                                <td style={{ verticalAlign: 'middle' }}>{leftPercentage}%</td>
+                                <td style={{ verticalAlign: 'middle' }}>{rightPercentage}%</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <th style={{ verticalAlign: 'middle' }}>불균형</th>
+                                <td style={{ verticalAlign: 'middle' }}>{unbalValue}%</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
--- src/components/dashboard/analysis/footPressDistributeAns.jsx
+++ src/components/dashboard/analysis/footPressDistributeAns.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import footPressIcon from '../../../assets/foot-icon.png';
 
 const FootPressDistributeAns = ({ leftFootPress, rightFootPress }) => {
     leftFootPress = { total: 307, average: 291, area: 7 };
@@ -7,34 +8,39 @@ const FootPressDistributeAns = ({ leftFootPress, rightFootPress }) => {
     return (
         <div className='footPressContainer'>
             <h4>발 압력 분포 분석</h4>
-            <div className='footPressData' style={{display: 'flex', flexDirection: 'row'}}>
+            <div className='footPressData' style={{ display: 'flex', flexDirection: 'row' }}>
                 <div className='footPressGraphic'>
-                    <img src="/path/to/foot-image.png" alt="Foot" />
+                    <img src={footPressIcon} width={'40%'} alt="Foot" />
                 </div>
-                <div>
-                    <table>
+                <div style={{ width: '100%' }}>
+                    <table style={{ width: '100%', textAlign: 'center', borderSpacing: '10px' }}>
+                        <colgroup>
+                            <col style={{ width: '50%' }} />
+                            <col style={{ width: '25%' }} />
+                            <col style={{ width: '25%' }} />
+                        </colgroup>
                         <thead>
                             <tr>
-                                <th></th>
-                                <th style={{backgroundColor: '#F7685B', borderRadius: '5px', color: '#ffffff'}}>왼발</th>
-                                <th style={{backgroundColor: '#2ED47A', borderRadius: '5px', color: '#ffffff'}}>오른발</th>
+                                <th style={{ width: '50%', verticalAlign: 'middle' }}></th>
+                                <th style={{ backgroundColor: '#F7685B', borderRadius: '5px', color: '#ffffff', width: '25%', verticalAlign: 'middle' }}>왼발</th>
+                                <th style={{ backgroundColor: '#2ED47A', borderRadius: '5px', color: '#ffffff', width: '25%', verticalAlign: 'middle' }}>오른발</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <th>총 압력 값</th>
-                                <td>{leftFootPress.total}</td>
-                                <td>{rightFootPress.total}</td>
+                                <th style={{ verticalAlign: 'middle' }}>총 압력 값</th>
+                                <td style={{ verticalAlign: 'middle' }}>{leftFootPress.total}</td>
+                                <td style={{ verticalAlign: 'middle' }}>{rightFootPress.total}</td>
                             </tr>
                             <tr>
-                                <th>압력 평균 값</th>
-                                <td>{leftFootPress.average}</td>
-                                <td>{rightFootPress.average}</td>
+                                <th style={{ verticalAlign: 'middle' }}>압력 평균 값</th>
+                                <td style={{ verticalAlign: 'middle' }}>{leftFootPress.average}</td>
+                                <td style={{ verticalAlign: 'middle' }}>{rightFootPress.average}</td>
                             </tr>
                             <tr>
-                                <th>면적 수</th>
-                                <td>{leftFootPress.area}</td>
-                                <td>{rightFootPress.area}</td>
+                                <th style={{ verticalAlign: 'middle' }}>면적 수</th>
+                                <td style={{ verticalAlign: 'middle' }}>{leftFootPress.area}</td>
+                                <td style={{ verticalAlign: 'middle' }}>{rightFootPress.area}</td>
                             </tr>
                         </tbody>
                     </table>
