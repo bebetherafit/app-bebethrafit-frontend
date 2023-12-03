c69a4a87b9f7bdeb75296be3acbb39f53bc031d1
Junho <jhshin9345@gmail.com>
2023-12-03 15:49:00 +0900

    updated admin

--- src/pages/workoutlog.jsx
+++ src/pages/workoutlog.jsx
@@ -1,86 +1,63 @@
 import React, { useState, useEffect } from "react";
+import ObesityIndex from "../components/workoutlog/ObesityIndex";
 import BodyBalance from "../components/workoutlog/BodyBalance";
 import FootType from "../components/workoutlog/FootType";
-import ObesityIndex from "../components/workoutlog/ObesityIndex";
-import "../styles/workoutlog.css";
 import axios from "axios";
 import config from "../config.json";
+import "../styles/workoutlog.css";
 
 const BACKEND_URL = config.macBackend;
-const ExerciseList = () => {
-  const [exercises, setExercises] = useState([]);
-  // const [completedExercises, setCompletedExercises] = useState({});
 
-  const [isLoading, setIsLoading] = useState(true);
+// WorkoutLog 컴포넌트
+function WorkoutLog() {
+    const [bmi, setBmi] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-      const fetchExercises = async () => {
-          setIsLoading(true);
-          try {
-              const token = localStorage.getItem('access_token');
-              console.log("Retrieved token:", token);
-              if (!token) {
-                  alert('인증시간이 만료되었습니다.');
-                  return;
-              }
-              const response = await axios.get(BACKEND_URL + '/api/analysis/bmi', {
-                  headers: {
-                      Authorization: `Bearer ${token}`
-                  }
-              });
-              localStorage.setItem('token', response.data.access_token);
-              setExercises(response.data);
-              console.log("오늘의 추천 운동 데이터 :", response.data);
-          } catch (error) {
-              console.error("API 호출 중 오류 발생:", error);
-              setExercises([]);
-          } finally {
-              setIsLoading(false);
-          }
-      };
-      fetchExercises();
-  }, []);
+    useEffect(() => {
+        const fetchBmi = async () => {
+            setIsLoading(true);
+            try {
+                const token = localStorage.getItem('access_token');
+                if (!token) {
+                    alert('인증시간이 만료되었습니다.');
+                    return;
+                }
+                const response = await axios.get(BACKEND_URL + '/api/analysis/bmi', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                setBmi(response.data);
+            } catch (error) {
+                console.error("BMI 데이터 호출 중 오류 발생:", error);
+                setBmi(0);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchBmi();
+    }, []);
 
-  // const handleCheckboxChange = (exerciseId) => {
-  //     setCompletedExercises({
-  //         ...completedExercises,
-  //         [exerciseId]: !completedExercises[exerciseId],
-  //     });
-  // };
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
-  if (!isLoading && exercises.length === 0) {
-      return <h2>오늘의 추천 운동이 없습니다</h2>;
-  }
+    if (!isLoading && bmi === 0) {
+        return <h2>오늘의 추천 운동이 없습니다</h2>;
+    }
 
-  return (
-      <div>
-          <h2>오늘의 운동 체크리스트</h2>
-
-      </div>
-  );
-};
-
-
-// WorkoutLog component
-function WorkoutLog () {
     return ( 
         <div className="dashboard-content">
             <div className="BodyGrowthAnalysisContainer">
                 <h1>금쪽이 신체 성장 종합 분석</h1>
                 <div className="BodyGrowthAnalysis">
-                    <ObesityIndex 
-                        obesityValue={0}
-                    />
+                    <ObesityIndex obesityValue={bmi} />
                     <FootType />
                     <BodyBalance />
                 </div>
             </div>
-            <div className="GrowthWorkOutContainer">
-                <h1>올바른 성장 습관 운동 리스트</h1>
-                <ExerciseList />
-            </div>
         </div>
-      );
+    );
 }
 
 export default WorkoutLog;
